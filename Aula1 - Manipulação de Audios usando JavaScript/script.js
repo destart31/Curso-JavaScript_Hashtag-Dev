243f8df9d6064415162f16665d086e1ef1ebbaa0
Arthur Gabriel <arthurgabriel31@hotmail.com>
2025-10-13 21:51:22 -0300

    Primeira aula sobre manipulação de audio usando JavaScript

---
+++ Aula1 - Manipulação de Audios usando JavaScript/script.js
@@ -0,0 +1,56 @@
+const botaoPlayPause = document.getElementById("play-pause");
+const botaoProximoCapitulo = document.getElementById("proximo");
+const botaoCapituloAnterior = document.getElementById("anterior");
+const audio = document.getElementById("audio-capitulo");
+const textoCapitulo = document.getElementById("capitulo");
+const qtdCapitulos = 10;
+
+let taTocando = false;
+let capituloAtual = 1;
+
+function tocarFaixa() {
+  audio.play();
+  taTocando = true;
+  botaoPlayPause.classList.add("tocando");
+}
+function pausarFaixa() {
+  audio.pause();
+  taTocando = false;
+  botaoPlayPause.classList.remove("tocando");
+}
+
+function tocarOuPausar() {
+  if (taTocando === true) {
+    pausarFaixa();
+  } else {
+    tocarFaixa();
+  }
+}
+
+function proximoCapitulo() {
+  pausarFaixa();
+  if (capituloAtual < qtdCapitulos) {
+    capituloAtual = capituloAtual + 1; // mesma coisa que escrever capituloAtual++;
+  } else {
+    capituloAtual = 1;
+  }
+  audio.src = "./audios/" + capituloAtual + ".mp3";
+  textoCapitulo.innerText = "Capitulo " + capituloAtual;
+  tocarFaixa();
+}
+
+function capituloAnterior() {
+  pausarFaixa();
+  if (capituloAtual === 1) {
+    capituloAtual = qtdCapitulos;
+  } else {
+    capituloAtual = capituloAtual - 1; // mesma coisa que escrever capituloAtual--;
+  }
+  audio.src = "./audios/" + capituloAtual + ".mp3";
+  textoCapitulo.innerText = "Capitulo " + capituloAtual;
+  tocarFaixa();
+}
+
+botaoPlayPause.addEventListener("click", tocarOuPausar);
+botaoProximoCapitulo.addEventListener("click", proximoCapitulo);
+botaoCapituloAnterior.addEventListener("click", capituloAnterior);
